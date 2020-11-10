4f65ae942ee51f99b2363aaba73a0856b17a8f0f
Paul Foos <paul.foos@umb.edu>
2020-11-10 09:27:11 -0500

    strip spaces from incoming text message

--- web/modules/custom/surveycampaign/src/TwilioIncomingService.php
+++ web/modules/custom/surveycampaign/src/TwilioIncomingService.php
@@ -20,8 +20,9 @@ class TwilioIncomingService
          //if($_REQUEST) $var = print_r($_REQUEST, true);
          //STOP, STOPALL, UNSUBSCRIBE, CANCEL, END, or QUIT
          //START, YES and UNSTOP
+         if($_REQUEST['Body']) {$bodytext = str_replace(' ', '',$_REQUEST['Body']);}
        
-        if($_REQUEST && (strtoupper($_REQUEST['Body']) == 'STOP' || strtoupper($_REQUEST['Body']) == 'STOPALL' || strtoupper($_REQUEST['Body']) == 'UNSUBSCRIBE' || strtoupper($_REQUEST['Body']) == 'CANCEL' || strtoupper($_REQUEST['Body']) == 'END' || strtoupper($_REQUEST['Body']) == 'QUIT' )) { 
+        if($_REQUEST && (strtoupper(str_replace(' ', '',$bodytext) == 'STOP' || strtoupper($bodytext == 'STOPALL' || strtoupper($bodytext == 'UNSUBSCRIBE' || strtoupper($bodytext == 'CANCEL' || strtoupper($bodytext == 'END' || strtoupper($bodytext == 'QUIT' )) { 
             $userphone = substr($_REQUEST['From'],2);
             $setinactive = \Drupal::service('surveycampaign.survey_users')->setUserStatus($userphone,'2');
             $email = $setinactive[0];
@@ -31,7 +32,7 @@ class TwilioIncomingService
 
             $sendemail = \Drupal::service('surveycampaign.twilio_coach')->twilioRespond($email,$firstname,$lastname,'stop');
         }
-        elseif($_REQUEST && (strtoupper($_REQUEST['Body']) == 'START' || strtoupper($_REQUEST['Body']) == 'YES' || strtoupper($_REQUEST['Body']) == 'UNSTOP' )) {
+        elseif($_REQUEST && (strtoupper($bodytext == 'START' || strtoupper($bodytext == 'YES' || strtoupper($bodytext == 'UNSTOP' )) {
             $userphone = substr($_REQUEST['From'],2);
             $setactive = \Drupal::service('surveycampaign.survey_users')->setUserStatus($userphone,'1');
             $email = $setactive[0];
