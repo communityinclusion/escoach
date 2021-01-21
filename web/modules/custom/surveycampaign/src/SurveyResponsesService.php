9f561234a2a54bf39b8157d6bc9dc0b3a26a61d6
Paul Foos <paul.foos@umb.edu>
2021-01-21 16:18:30 -0500

    survey data

--- web/modules/custom/surveycampaign/src/SurveyResponsesService.php
+++ web/modules/custom/surveycampaign/src/SurveyResponsesService.php
@@ -98,83 +98,8 @@ class SurveyResponsesService
         } else {
             return false;
         }
-        
-
-        
-        // return $userarray;
     }
-    public function handleSuspendDates($userphone,$startdate = null,$enddate = null) {
-        $today = new DateTime();
-        $today = $today->format('Y-m-d');
-
-        $storage = \Drupal::entityTypeManager()->getStorage('profile')
-            ->loadByProperties([
-                'type' => 'survey_participants',
-                'field_cell_phone' => $userphone,
-            ]);
-        
-        foreach($storage as $profile) {
-            if($userphone == preg_replace('/\D+/', '',$profile->get('field_cell_phone')->value) && $startdate && $enddate) {
-
-                $profile->set('field_partic_suspension_dates', array(
-                    'value' => $startdate,
-                    'end_value' => $enddate,
-                    ));
-                $profile->save();
-            } elseif ($userphone == preg_replace('/\D+/', '',$profile->get('field_cell_phone')->value) && !$startdate && !$enddate)
-            {
-                $suspension = $profile->get('field_partic_suspension_dates')->value ? $profile->get('field_partic_suspension_dates')->value : null ;
-                $suspension_end =  $profile->get('field_partic_suspension_dates')->end_value ? $profile->get('field_partic_suspension_dates')->end_value : null;
-                $inactive = $profile->get('field_set_surveys_to_inactive')->value == '2' ? 2 : null;
-                return array($suspension,$suspension_end,$inactive);
-               
-            }
-        }
-
-    }
-    public function checkInactive($userphone,$lastname) {
-        $storage = \Drupal::entityTypeManager()->getStorage('profile')
-            ->loadByProperties([
-                'type' => 'survey_participants',
-                'field_cell_phone' => $userphone,
-            ]);
-        
-        foreach($storage as $profile) {
-            echo "Check Inactive values: " . $userphone . " " . $lastname . " " . $profile->get('field_set_surveys_to_inactive')->value;
-            
-            if($userphone == preg_replace('/\D+/', '',$profile->get('field_cell_phone')->value) && $lastname == $profile->get('field_survey_last_name')->value && $profile->get('field_set_surveys_to_inactive')->value == '2') {
-               // echo "Confirm Inactive values: " . $profile->get('field_cell_phone')->value . " " . $profile->get('field_survey_last_name')->value . " " . $profile->get('field_set_surveys_to_inactive')->value;
-            return true;
-            }
-            else 
-            return false;
-        }
-    }
-
-    public function setUserStatus($userphone,$setstatus) {
-
-        $storage = \Drupal::entityTypeManager()->getStorage('profile')
-            ->loadByProperties([
-                'type' => 'survey_participants',
-                'field_cell_phone' => $userphone,
-            ]);
-        
-        foreach($storage as $profile) {
-            if($userphone == preg_replace('/\D+/', '',$profile->get('field_cell_phone')->value) && $profile->get('field_set_surveys_to_inactive')->value != "$setstatus") {
-                $user = $profile->getOwnerId();
-                $userobj = \Drupal\user\Entity\User::load($user);
-                $useremail = $userobj->getEmail();
-                $firstname = $profile->get('field_survey_first_name')->value ? $profile->get('field_survey_first_name')->value : '';
-                $lastname = $profile->get('field_survey_last_name')->value ? $profile->get('field_survey_last_name')->value : '';
-                $profile->set('field_set_surveys_to_inactive', array(
-                    'value' => "$setstatus"));
-                $profile->save();
-                return array($useremail,$firstname,$lastname);
-
-            }
-        }
-
-    }
+   
 
     
 }
