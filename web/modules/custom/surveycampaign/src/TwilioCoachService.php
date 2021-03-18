851a420057c2be6a8982e41e81b91d5e019e9f90
Paul Foos <paul.foos@umb.edu>
2021-03-18 15:04:58 -0400

    testing again

--- web/modules/custom/surveycampaign/src/TwilioCoachService.php
+++ web/modules/custom/surveycampaign/src/TwilioCoachService.php
@@ -458,8 +458,6 @@ class TwilioCoachService
             $output= json_decode($output);
             //$didnotreply = false;
             $inactive = false;
-            $logmessage = "Phone: " . $mobilephone . " Lastname: " . $lastname;
-            \Drupal::logger('surveycampaign')->notice($logmessage);
             $inactive = \Drupal::service('surveycampaign.survey_users')->checkInactive($mobilephone,$lastname);
             $didnotreply = !empty($recentcampaigns) ?intval($this->checkNonReplies($surveyid,$mobilephone,$fullname,$recentcampaigns)) : false;
             if($didnotreply >= $limit && !$inactive) { $sendwarning = $this->mailNonReplyer($email,$firstname,$lastname,$mobilephone);}
