946c0903c0846cea37d72c0fb92f5941605740fd
Paul Foos <paul.foos@umb.edu>
2021-04-30 08:03:45 -0400

    conditional field on user profile form using js

--- web/themes/custom/escoach_boot4/js/global.js
+++ web/themes/custom/escoach_boot4/js/global.js
@@ -16,6 +16,19 @@
           if(!$('#edit-survey-participants-profiles-0-entity-field-other-provider-wrapper').hasClass('showOther')) $('#edit-survey-participants-profiles-0-entity-field-other-provider-wrapper').addClass('showOther');
         } else {
           if($('#edit-survey-participants-profiles-0-entity-field-other-provider-wrapper').hasClass('showOther')) $('#edit-survey-participants-profiles-0-entity-field-other-provider-wrapper').removeClass('showOther');
+          $('input#edit-survey-participants-profiles-0-entity-field-other-provider-0-value').val("");
+        }
+
+
+      });
+      $( "select#edit-field-provider",context ).on('change',function() {
+        var provChoice = $(this).val();
+        
+        if (provChoice == "6") { 
+          if(!$('#edit-field-other-provider-wrapper').hasClass('showOther')) $('#edit-field-other-provider-wrapper').addClass('showOther');
+        } else {
+          if($('#edit-field-other-provider-wrapper').hasClass('showOther')) $('#edit-field-other-provider-wrapper').removeClass('showOther');
+          $('input#edit-field-other-provider-0-value').val("");
         }
 
 
@@ -27,6 +40,17 @@
           if(!$('#edit-survey-participants-profiles-0-entity-field-other-provider-wrapper').hasClass('showOther')) $('#edit-survey-participants-profiles-0-entity-field-other-provider-wrapper').addClass('showOther');
         } else {
           if($('#edit-survey-participants-profiles-0-entity-field-other-provider-wrapper').hasClass('showOther')) $('#edit-survey-participants-profiles-0-entity-field-other-provider-wrapper').removeClass('showOther');
+        }
+
+
+      });
+      $( "select#edit-field-provider",context ).once('providerchoiceedit').each(function() {
+        var provChoice = $(this).val();
+        
+        if (provChoice == "6") { 
+          if(!$('#edit-field-other-provider-wrapper').hasClass('showOther')) $('#edit-field-other-provider-wrapper').addClass('showOther');
+        } else {
+          if($('#edit-field-other-provider-wrapper').hasClass('showOther')) $('#edit-field-other-provider-wrapper').removeClass('showOther');
         }
 
 
