72bf61016b3e65582d036793697d1385fea1a5b8
Martin Gottlieb <marting@cobblestone.consulting>
2021-05-30 16:46:09 -0400

    [#25] Initial version of survey_dashboard module

---
+++ web/modules/custom/survey_dashboard/src/Query/What.php
@@ -0,0 +1,17 @@
+<?php
+namespace Drupal\survey_dashboard\Query;
+
+class What extends BaseQuery {
+
+  const QUESTION_ID = 483;
+
+  protected $valueAliasMap = [
+    'JobSeeker' => 11658,
+    'Finding'=> 11659,
+    'After' => 11660,
+    'Before'=> 11661,
+    'Day' => 11662,
+    'Staff'=> 11663,
+  ];
+
+}
