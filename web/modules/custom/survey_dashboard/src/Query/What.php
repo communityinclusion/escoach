991c6ca4cabdd9f45307588e6bc14056f5abb7f2
Michael Anello <manello@gmail.com>
2021-06-10 13:42:33 -0400

    Re-added survey_dashboard stuff.

---
+++ web/modules/custom/survey_dashboard/src/Query/What.php
@@ -0,0 +1,9 @@
+<?php
+namespace Drupal\survey_dashboard\Query;
+
+class What extends BaseQuery {
+
+  const VID = 'what';
+  const QUESTION_ID = 483;
+
+}
---
+++ web/modules/custom/survey_dashboard/src/Query/Where.php
@@ -0,0 +1,9 @@
+<?php
+namespace Drupal\survey_dashboard\Query;
+
+class Where extends BaseQuery {
+
+  const VID = 'where';
+  const QUESTION_ID = 482;
+
+}
