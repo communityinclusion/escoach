f377f45ed05b24050c2206dbac404ab4562e1132
Martin Gottlieb <marting@cobblestone.consulting>
2021-06-18 11:50:43 -0400

    [#11] Fix JS logic for unselecting who/where lists

--- web/modules/custom/survey_dashboard/js/survey_dashboard.js
+++ web/modules/custom/survey_dashboard/js/survey_dashboard.js
@@ -9,11 +9,11 @@
 
   Drupal.behaviors.surveyDashboard = {
     attach: function (context, settings) {
-      $('input[name="who"]').once('who-select').on('click', function (evt) {
+      $('input[name="who"]').once('who-select').on('change', function (evt) {
         $('input[name="where"]').prop('checked', false);
       });
 
-      $('input[name="where"]').once('where-select').on('click', function (evt) {
+      $('input[name="where"]').once('where-select').on('change', function (evt) {
         $('input[name="who"]').prop('checked', false);
       });
     }
