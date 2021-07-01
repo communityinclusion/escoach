c08fc15bdec6bb7880a9900a3b2edafec07b529f
Martin Gottlieb <marting@cobblestone.consulting>
2021-07-01 09:44:25 -0400

    [#38] Disable trends timeframe options when no What options are selected.

--- web/modules/custom/survey_dashboard/js/survey_dashboard.js
+++ web/modules/custom/survey_dashboard/js/survey_dashboard.js
@@ -16,6 +16,26 @@
       $('select[name="where"]').once('where-select').on('change', function (evt) {
         $('select[name="who"]').val('_none');
       });
+
+      $('#edit-timeframe').once('disable-trends-options').each(function () {
+        Drupal.behaviors.surveyDashboard.checkWhats();
+      });
+
+      $('input[name^="what"]').once('update-timeframe').on('click', function () {
+        Drupal.behaviors.surveyDashboard.checkWhats();
+      });
+    },
+
+    checkWhats: function () {
+      if ( $('input:checked[name^="what"]').length > 0 ) {
+        $('#edit-timeframe option[value="quarterly"]').prop('disabled', '');
+        $('#edit-timeframe option[value="monthly"]').prop('disabled', '');
+      }
+      else {
+        $('#edit-timeframe option[value="quarterly"]').prop('disabled', 'disabled');
+        $('#edit-timeframe option[value="monthly"]').prop('disabled', 'disabled');
+        $('#edit-timeframe').val('up-to-date');
+      }
     }
   };
 
