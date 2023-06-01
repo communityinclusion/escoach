6e8222713b76438a305db644be3dd1fba96a5251
Paul Foos <paul.foos@umb.edu>
2023-05-31 20:23:48 -0400

    testing error with Twilio stop messages

--- web/modules/custom/surveycampaign/src/TwilioIncomingService.php
+++ web/modules/custom/surveycampaign/src/TwilioIncomingService.php
@@ -41,8 +41,9 @@ class TwilioIncomingService
 
             $sendemail = \Drupal::service('surveycampaign.twilio_coach')->twilioRespond($email,$firstname,$lastname,'stop');
             return new Response(
-        'Message sent.',
-         Response::HTTP_OK
+              'Message sent.',
+              Response::HTTP_OK
+           );
         }
 
         elseif($_REQUEST && (strtoupper($bodytext) == 'START' || strtoupper($bodytext) == 'YES' || strtoupper($bodytext) == 'UNSTOP' )) {
@@ -60,9 +61,9 @@ class TwilioIncomingService
             $lastname = $setactive[2];
             $sendemail = \Drupal::service('surveycampaign.twilio_coach')->twilioRespond($email,$firstname,$lastname,'start');
             return new Response(
-        'Message sent.',
-         Response::HTTP_OK
-    );
+              'Message sent.',
+              Response::HTTP_OK
+            );
 
         }
       else return;
