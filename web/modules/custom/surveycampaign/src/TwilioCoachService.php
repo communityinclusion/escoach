fc32f518908843c57146a23f2bbed87c9db273e3
Paul Foos <paul.foos@umb.edu>
2023-06-20 08:47:32 -0400

    restoring warning texts and emails

--- web/modules/custom/surveycampaign/src/TwilioCoachService.php
+++ web/modules/custom/surveycampaign/src/TwilioCoachService.php
@@ -927,12 +927,12 @@ class TwilioCoachService
             $sendTime = date('H:i', strtotime('12:00'));
             if(($warningmode == '2' || $warningmode == '3') && $isprimary) {
                 \Drupal::logger('surveycampaign alert')->notice('Going to mail manager');
-              //  $result = $mailManager->mail($module, $key, $to, $langcode, $params, $siteemail, $send);
+                $result = $mailManager->mail($module, $key, $to, $langcode, $params, $siteemail, $send);
             }
-          //  if(($warningmode == '1' || $warningmode == '3') && $isprimary && $nowTime >= $sendTime ) {
-            //    $this->twilioCall($mobilephone,"$firstname $lastname",$invitelink,$textno,$dayno,$warningdays,$isprimary);
+            if(($warningmode == '1' || $warningmode == '3') && $isprimary && $nowTime <= $sendTime ) {
+                $this->twilioCall($mobilephone,"$firstname $lastname",$invitelink,$textno,$dayno,$warningdays,$isprimary);
 
-          //  }
+            }
         }
     }
     public function twilioRespond($email,$firstname,$lastname,$responseaction) {
