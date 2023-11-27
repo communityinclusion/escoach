2a3594252fe89fab0a60508592e17e0a0d2c524f
Martin Gottlieb <martin@cobblestone.consulting>
2023-11-27 16:05:22 -0500

    98: Update usage of .once() in provider js

--- web/modules/custom/es_homepage/js/provider-select.js
+++ web/modules/custom/es_homepage/js/provider-select.js
@@ -9,11 +9,13 @@
 
   Drupal.behaviors.homepageProviderSelect = {
     attach: function (context, settings) {
-      $('#provider-select').once('select-provider').on('change', function (evt) {
-        if ($(this).val()) {
-          let currentUrl = window.location.pathname;
-          window.location.href = currentUrl + '?provider=' + $(this).val() + '&t=' + Date.now();
-        }
+      $(once( 'select-provider', '#provider-select', context)).each(function () {
+        $(this).on('change', function (evt) {
+          if ($(this).val()) {
+            let currentUrl = window.location.pathname;
+            window.location.href = currentUrl + '?provider=' + $(this).val() + '&t=' + Date.now();
+          }
+        });
       });
     }
   };
