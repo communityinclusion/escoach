a51b3b508972db0051082af4e452707657c3f74d
Paul Foos <paul.foos@umb.edu>
2024-01-05 09:05:19 -0500

    fixed once library error in surveycampaign js

--- web/modules/custom/surveycampaign/js/surveycampaign.js
+++ web/modules/custom/surveycampaign/js/surveycampaign.js
@@ -10,7 +10,7 @@
 
     Drupal.behaviors.surveyconfig = {
       attach: function (context, settings) {
-        $('input.toggleHeading', context).once('scanFields').each(function() {
+        once('scanFields','input.toggleHeading', context).forEach(function(value,i) {
             var inputName = $(this).attr('name');
                 var checkedVal = $('input[name="' + inputName + '"]:checked').val();
                 var optionalText = $(this).closest('.inner-fieldset').find('.customHeading').attr('id');
@@ -20,26 +20,27 @@
 
 
 
-        $('input.toggleHeading', context).once('toggleHeading').change(function() {
+        once('toggleHeading','input.toggleHeading', context).foreach(function(value,i) {
+                $(value).change(function(){
+                  var inputName = $(this).attr('name');
+                  var checkedVal = $('input[name="' + inputName + '"]:checked').val();
+                  var optionalText = $(this).closest('.inner-fieldset').find('.customHeading').attr('id');
+                  if(checkedVal == '4') {
+                  if($('#' + optionalText).hasClass('hideOption')) {$('#' + optionalText).removeClass('hideOption');}
 
-                var inputName = $(this).attr('name');
-                var checkedVal = $('input[name="' + inputName + '"]:checked').val();
-                var optionalText = $(this).closest('.inner-fieldset').find('.customHeading').attr('id');
-                if(checkedVal == '4') {
-                if($('#' + optionalText).hasClass('hideOption')) {$('#' + optionalText).removeClass('hideOption');}
+                  } else { $('#' + optionalText).val('');
+                       if(!$('#' + optionalText).hasClass('hideOption')) {$('#' + optionalText).addClass('hideOption');
 
-                } else { $('#' + optionalText).val('');
-                     if(!$('#' + optionalText).hasClass('hideOption')) {$('#' + optionalText).addClass('hideOption');
-
-                    }
-                }
+                      }
+                  }
+              });
 
 
           });
 
       }
     };
-  
+
 
 
 
