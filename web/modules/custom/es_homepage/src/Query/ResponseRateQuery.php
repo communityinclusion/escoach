a0a96b83b9e515aab62e44516311f79abfa7b0e3
Martin Gottlieb <martin@cobblestone.consulting>
2024-04-19 10:44:53 -0400

    128: Only apply min regcode filter to ALL in response rate queries

--- web/modules/custom/es_homepage/src/Query/ResponseRateQuery.php
+++ web/modules/custom/es_homepage/src/Query/ResponseRateQuery.php
@@ -21,9 +21,12 @@ class ResponseRateQuery extends HomePageQuery {
     $this->query->addJoin('LEFT', 'surveycampaign_results', 'results', 'mailer.contactid = results.contact_id');
     $this->setDateRange($year, $month, 'mailer.senddate');
     $this->query->condition('mailer.surveyid', 5420562);
-    $this->query->condition('results.regcode', 10000, '>=');
     $this->email = $email;
     $this->provider = $provider;
+  }
+
+  public function addMinRegCode($regCode = 10000) {
+    $this->query->condition('results.regcode', $regCode, '>=');
   }
 
   public function addMe() {
