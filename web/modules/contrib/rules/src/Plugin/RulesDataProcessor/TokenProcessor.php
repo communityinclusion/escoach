e50d0069553861e37e3c1495dc0415f2a766a386
Paul Foos <paul.foos@umb.edu>
2025-02-13 13:31:19 -0500

    style change to hide autologin url field on user form and hack to rules module to prevent crash on user creation

--- web/modules/contrib/rules/src/Plugin/RulesDataProcessor/TokenProcessor.php
+++ web/modules/contrib/rules/src/Plugin/RulesDataProcessor/TokenProcessor.php
@@ -65,6 +65,10 @@ class TokenProcessor extends PluginBase implements DataProcessorInterface, Conta
    * {@inheritdoc}
    */
   public function process($value, ExecutionStateInterface $rules_state) {
+    if (empty($value)) {
+	    return $value;
+	  }
+
     $data = [];
     $placeholders_by_data = $this->placeholderResolver->scan($value);
     foreach ($placeholders_by_data as $variable_name => $placeholders) {
